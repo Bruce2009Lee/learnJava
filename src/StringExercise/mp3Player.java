162442b5377bc8d89c7ec21067794759e0f10a66
zhonglianli2009 <zhonglianli2009@163.com>
2017-03-06 22:37:20 +0800

    latered

--- src/StringExercise/mp3Player.java
+++ src/StringExercise/mp3Player.java
@@ -12,7 +12,7 @@ public class mp3Player {
 		for(int i = 0 ; i < num; i++){
 			System.out.print(arr[begin + i]+ " ");
 		}
-		
+		System.out.println("");
 	}
 	
 	public static void main(String[] args) {
@@ -27,19 +27,96 @@ public class mp3Player {
 			
 			char[]  cmd = scan.nextLine().toCharArray();
 			int head = 0,current = 0;
-			int end = head + sum;
+			int end = head + sum -1;
 			for(int i = 0 ; i < cmd.length; i++){
-				if(cmd[i] == 'D'){
-					if(i == end){
-						printCurrent(songs, head,1);
+				if(sum <= 4){
+					printCurrent(songs, head,sum);
+					if(cmd[i] == 'D'){
+						if(i == end){
+							
+							current = 0 ;
+							if(i == cmd.length - 1){
+								printCurrent(songs, head,1);
+							}
+						}
+						else{
+							
+							current++;
+							if(i == cmd.length - 1){
+								printCurrent(songs, current,1);
+							}
+						}
 					}
-					else{
-						printCurrent(songs, current,1);
-						current++;
+					else{	//cmd[i] == 'U'
+						
+						if(i == head){
+							if(i == cmd.length - 1){
+								printCurrent(songs, end,1);
+							}
+							current = sum -1 ;
+						}
+						else{
+							current--;
+							if(i == cmd.length - 1){
+								printCurrent(songs, current,1);
+							}
+						}
 					}
 				}
-				else{
-					
+				else{//cmd.length > 4
+					if(cmd[i] == 'D'){
+						if(i == end){
+							current = 0 ;
+							if(i == cmd.length - 1){
+								printCurrent(songs, head,4);
+								printCurrent(songs, head,1);
+							}
+							
+						}
+						else{							
+							current++;
+							for(int j = 0 ; j < sum/4; j++){
+								if( current>=(4*j +1)&&(current <=4*j + 4) ){
+									if(i == cmd.length - 1){
+										if( sum%4 != 0 && j + 1 ==sum/4){
+											printCurrent(songs, 4*j,4);
+										}
+										else{
+											printCurrent(songs, 4*j,4);
+										}
+										printCurrent(songs, current,1);
+									}
+								}
+							}
+
+						}
+					}
+					else{	//cmd[i] == 'U'
+						
+						if(i == head){
+							if(i == cmd.length - 1){
+								printCurrent(songs, end,1);
+								printCurrent(songs, end-3,4);
+							}
+							current = sum -1 ;
+						}
+						else{
+							current--;
+							for(int j = 0 ; j < sum/4; j++){
+								if(( current>=(4*j +1))&&(current <=(4*j + 4)) ){
+									if(i == cmd.length - 1){
+										if( sum%4 != 0 && j + 1 ==sum/4){
+											printCurrent(songs, 4*j,4);
+										}
+										else{
+											printCurrent(songs, 4*j,4);
+										}
+											printCurrent(songs, current,1);
+									}
+								}
+							}
+						}
+					}
 				}
 			}
 			System.out.print("Done");                              
